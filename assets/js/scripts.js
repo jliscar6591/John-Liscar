b0cb35a3cc3f2a0078191bf18f2634534ebd7d5d
john liscar <john.liscar@gmail.com>
2018-01-30 12:38:26 -0500

    contact form js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+$('#contact-form').submit(function(e) {
+  var first_name = document.getElementById('first_name'),
+      last_name  = document.getElementById('last_name'),
+      email  = document.getElementById('email'),
+      text  = document.getElementById('text');
+
+if (!first_name.value || !last_name.value || !email.value || !text.value) {
+  throw error;
+} else {
+  $.ajax({
+    url: "https://formspree.io/john.liscar@gmail.com", 
+    method: "POST",
+    data: $(this).serialize(),
+    dataType: "json"
+});
+  e.preventDefault()|
+  $(this).get(0).reset()
+  alert("Message Sent");
+}
+})
